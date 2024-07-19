f43727eeca76f5d22e616fe8159b782e111173c0
Snehal Salve <69302530+SnehalSalve@users.noreply.github.com>
2024-07-19 12:57:16 +0530

    practice

--- Student.java
+++ Student.java
@@ -1,60 +1,48 @@
-package com.student.entity;
+package com.spring.demo;
 
 public class Student {
-	private int student_id;
-	private String student_name;
-	private String student_subject;
-	private float student_percentage;
-	private String student_city;
+
+	private int id;
+	private String name;
+	private String city;
+	
+	private Dept dept;
 	public Student() {
 		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Student(int student_id, String student_name, String student_subject, float student_percentage,
-			String student_city) {
-		super();
-		this.student_id = student_id;
-		this.student_name = student_name;
-		this.student_subject = student_subject;
-		this.student_percentage = student_percentage;
-		this.student_city = student_city;
-	}
-	public int getStudent_id() {
-		return student_id;
-	}
-	public void setStudent_id(int student_id) {
-		this.student_id = student_id;
-	}
-	public String getStudent_name() {
-		return student_name;
-	}
-	public void setStudent_name(String student_name) {
-		this.student_name = student_name;
-	}
-	public String getStudent_subject() {
-		return student_subject;
-	}
-	public void setStudent_subject(String student_subject) {
-		this.student_subject = student_subject;
-	}
-	public float getStudent_percentage() {
-		return student_percentage;
-	}
-	public void setStudent_percentage(float student_percentage) {
-		this.student_percentage = student_percentage;
-	}
-	public String getStudent_city() {
-		return student_city;
-	}
-	public void setStudent_city(String student_city) {
-		this.student_city = student_city;
-	}
-	@Override
-	public String toString() {
-		return "Student [student_id=" + student_id + ", student_name=" + student_name + ", student_subject="
-				+ student_subject + ", student_percentage=" + student_percentage + ", student_city=" + student_city
-				+ "]";
 	}
 	
+	public Student(int id, String name, String city, Dept dept) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.dept = dept;
+	}
+
+	public Dept getDept() {
+		return dept;
+	}
+
+	public void setDept(Dept dept) {
+		this.dept = dept;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 }
--- Test.java
+++ Test.java
@@ -1,15 +1,17 @@
-package com.singlton.demo;
+package com.spring.demo;
 
 public class Test {
 
 	public static void main(String[] args) {
+//
+//		Dept d1 = new Dept(1,"IT");
+//		Student s1 = new Student(101,"mansi","pune",d1);
+//		
+//		System.out.println(s1);
 		
-		Data d1 =  Data.demo();
-		Data d2 = Data.demo();
-		
-		System.out.println("d1 : "+d1.hashCode());
-		System.out.println("d2 : "+d2.hashCode());
-
+		Student S1 = new Student();
+		S1.setId(101);
+		System.out.println(S1.getId());
 	}
-	
+
 }
